b525193ba50bfff4307b554d224245f3ab2c4723
Muhammad Yasser <mhmdibnyasr@gmail.com>
2026-01-22 04:30:46 +0200

    treesitter switched to master branch

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,14 +7,26 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+				require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				-- null_ls.builtins.diagnostics.ruff,
 				null_ls.builtins.formatting.black,
-				-- null_ls.builtins.diagnostics.yamllint,
-				require("none-ls.diagnostics.eslint_d"),
+				-- null_ls.builtins.completion.spell,
+				-- require("none-ls.diagnostics.eslint_d"), -- requires none-ls-extras.nvim
 			},
 		})
+
+		-- null_ls.setup({
+		-- 	sources = {
+		-- 		null_ls.builtins.formatting.stylua,
+		-- 		null_ls.builtins.formatting.eslint_d,
+		-- 		null_ls.builtins.formatting.prettier,
+		-- 		-- null_ls.builtins.diagnostics.ruff,
+		-- 		null_ls.builtins.formatting.black,
+		-- 		-- null_ls.builtins.diagnostics.yamllint,
+		-- 		require("none-ls.diagnostics.eslint"),
+		-- 	},
+		-- })
 
 		vim.keymap.set("n", "<leader>i", vim.lsp.buf.format, {})
 	end,
